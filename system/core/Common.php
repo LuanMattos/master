bedae9ca63ca5196e08ecbecb40e66492997ffd7
luanmattos <patrickluan.matos@gmail.com>
2019-03-02 14:34:29 -0300

    sistema vazio inclusao anotacoes e licensa php storm

--- system/core/Common.php
+++ system/core/Common.php
@@ -1,30 +1,6 @@
 <?php
 /**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+
  *
  * @package	CodeIgniter
  * @author	EllisLab Dev Team
@@ -34,6 +10,7 @@
  * @link	https://codeigniter.com
  * @since	Version 1.0.0
  * @filesource
+ * 
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -72,6 +49,28 @@ if ( ! function_exists('is_php'))
 		return $_is_php[$version];
 	}
 }
+
+// ------------------------------------------------------------------------
+if ( ! function_exists('date_to_br')) {
+    /**
+     * Converte valores de data no formato time stamp para o padrão brasileiro
+     *
+     * @param	string
+     * @return	bool	TRUE if the current version is $version or higher
+     */
+    function date_to_br($date_timestamp){
+        echo $date_timestamp;
+
+
+
+
+
+
+
+    }
+}
+// ------------------------------------------------------------------------
+
 
 // ------------------------------------------------------------------------
 
@@ -220,6 +219,7 @@ if ( ! function_exists('is_loaded'))
 
 		return $_is_loaded;
 	}
+
 }
 
 // ------------------------------------------------------------------------
