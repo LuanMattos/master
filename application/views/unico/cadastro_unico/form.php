dc2079a23445e70457c41fae88dafe83689bca02
luanmattos <patrickluan.matos@gmail.com>
2019-09-15 09:44:26 -0300

    VueJs em funcionamento, modal sem cor branca no texto title-bar app.alertas ok

---
+++ application/views/unico/cadastro_unico/form.php
@@ -0,0 +1,24 @@
+<div>
+    <form action="<?= site_url("unico/cadastro_unico/Cadastro_unico/index") ?>" method="POST" id="form-cadastro-unico-container">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Buscar:</span>
+            </div>
+            <input type="text" class="form-control" id="buscar">
+            <div class="input-group-append">
+                <input type="submit" class="btn btn-info buscar" value="Buscar">
+                <button type="button" class="btn btn-info novo "><i class="far fa-file"></i> Novo</button>
+            </div>
+        </div>
+    </form>
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+
