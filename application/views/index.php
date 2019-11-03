48180cb3a007ee9d2501fbd07c2126af31de0580
root <root@localhost.localdomain>
2019-11-03 01:23:29 -0300

    mudar urll base para site urll

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,7 @@
 <head>
-    <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/js/jquery-3.3.1.slim.min.js"></script>
-    <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/dist/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/dist/css/bootstrap.css">
+    <script type="text/javascript" src="<?= site_url("application/assets/js/libs/bootstrap-4.1.3/js/jquery-3.3.1.slim.min.js") ?>"></script>
+    <script type="text/javascript" src="<?= site_url("application/assets/js/libs/bootstrap-4.1.3/dist/js/bootstrap.js")?>"></script>
+    <link rel="stylesheet" href="<?= site_url("application/assets/js/libs/bootstrap-4.1.3/dist/css/bootstrap.css") ?>">
     <link rel="stylesheet" href="<?= site_url("application/assets/js/libs/icons/fontawesome-free-5.10.2-web/css/all.css") ?>">
     <link href="<?= site_url("application/assets/css/tailwindcss.css") ?>" rel="stylesheet">
 
@@ -9,7 +9,7 @@
 <style>
     .bg {
         background-color: #3e3e3f;
-        background-image: url(<?= URL_RAIZ() . 'application/assets/images/svg/graph-paper/texture.svg' ?>);
+        background-image: url(<?= site_url('application/assets/images/svg/graph-paper/texture.svg')  ?>);
     }
 
 </style>
@@ -337,15 +337,15 @@
     </div>
 </div>
 </body>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>js/index.js"></script>
+<script type="text/javascript" src="<?=  site_url("js/index.js") ?>"></script>
 <!-----------------------------------------jquery-default-sistema------------------------------------------------------>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery-3.4.1/jquery.js"></script>
+<script type="text/javascript" src="<?= site_url("application/assets/js/libs/jquery-3.4.1/jquery.js") ?>"></script>
 <!------------------------------------------jquery-ui-p/-modal--------------------------------------------------------->
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery-1.12.4/jquery-1.12.4.js"></script>
+<script type="text/javascript" src="<?= site_url("application/assets/js/libs/jquery-1.12.4/jquery-1.12.4.js") ?>"></script>
 <!----------------------------------------------jquery-ui-------------------------------------------------------------->
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery-ui-1.12.1/jquery-ui.js"></script>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/vue.js"></script>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js.js"></script>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/mascaras.js"></script>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery.mask.js"></script>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/icons/fontawesome-free-5.10.2-web/js/all.js"></script>
+<script type="text/javascript" src="<?= site_url("application/assets/js/libs/jquery-ui-1.12.1/jquery-ui.js") ?> "></script>
+<script type="text/javascript" src="<?= site_url("application/assets/js/libs/vue.js") ?>"></script>
+<script type="text/javascript" src="<?= site_url("application/assets/js.js") ?>" ></script>
+<script type="text/javascript" src="<?= site_url("application/assets/mascaras.js") ?> "></script>
+<script type="text/javascript" src="<?= site_url("application/assets/js/libs/jquery.mask.js") ?> "></script>
+<script type="text/javascript" src="<?= site_url("application/assets/js/libs/icons/fontawesome-free-5.10.2-web/js/all.js") ?> "></script>
