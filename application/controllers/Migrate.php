46a5843018ce7dc7df79af10e7cd15847bd9b6e4
root <root@localhost.localdomain>
2019-11-03 17:48:10 -0300

    save db query migrate

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -22,7 +22,7 @@ class Migrate extends CI_Controller{
         $this->db->trans_start();
 
 
-        pg_query("CREATE TABLE if not exists usuarios(
+        $this->db->query("CREATE TABLE if not exists usuarios(
                                 codigo       serial NOT NULL,
                                 login        varchar(500),
                                 senha        varchar(1000),
@@ -31,21 +31,21 @@ class Migrate extends CI_Controller{
                                 updated_at   timestamptz default now(),
                                 primary key (codigo)
                                 )");
-        pg_query("CREATE OR REPLACE FUNCTION  trigger_set_timestamp() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = NOW();
+        $this->db->query("CREATE OR REPLACE FUNCTION  trigger_set_timestamp() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = NOW();
                                 RETURN NEW;
                                 END;
                                 $$ LANGUAGE plpgsql");
-        pg_query("DROP      TRIGGER if exists  set_timestamp ON usuarios");
-        pg_query("CREATE    TRIGGER set_timestamp
+        $this->db->query("DROP      TRIGGER if exists  set_timestamp ON usuarios");
+        $this->db->query("CREATE    TRIGGER set_timestamp
                                 BEFORE UPDATE ON usuarios
                                 FOR EACH ROW
                                 EXECUTE PROCEDURE trigger_set_timestamp()");
-        pg_query("ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS  __ci_last_regenerate numeric(500)");
-        pg_query("ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS logado boolean");
-        pg_query("ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS session_coo varchar(10000)");
-        pg_query("ALTER TABLE usuarios add column IF NOT EXISTS email varchar(1000)");
-        pg_query("ALTER TABLE usuarios add column IF NOT EXISTS datanasc date");
-        pg_query("ALTER TABLE usuarios add column IF NOT EXISTS telcel varchar(1000)");
+        $this->db->query("ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS  __ci_last_regenerate numeric(500)");
+        $this->db->query("ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS logado boolean");
+        $this->db->query("ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS session_coo varchar(10000)");
+        $this->db->query("ALTER TABLE usuarios add column IF NOT EXISTS email varchar(1000)");
+        $this->db->query("ALTER TABLE usuarios add column IF NOT EXISTS datanasc date");
+        $this->db->query("ALTER TABLE usuarios add column IF NOT EXISTS telcel varchar(1000)");
 
         $transaction = $this->db->trans_complete();
         if(!$transaction){
