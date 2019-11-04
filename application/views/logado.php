f62dce4ebbc16b6fb6a21fb57e7119442d843974
root <root@localhost.localdomain>
2019-11-03 22:55:36 -0300

    save

--- application/views/logado.php
+++ application/views/logado.php
@@ -1,80 +1,99 @@
 <head>
-    <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/js/jquery-3.3.1.slim.min.js"></script>
-    <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/dist/js/bootstrap.js"></script>
+    <script type="text/javascript"
+            src="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/js/jquery-3.3.1.slim.min.js"></script>
+    <script type="text/javascript"
+            src="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/dist/js/bootstrap.js"></script>
     <link rel="stylesheet" href="<?= URL_RAIZ() ?>application/assets/js/libs/bootstrap-4.1.3/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="<?= site_url("application/assets/js/libs/icons/fontawesome-free-5.10.2-web/css/all.css") ?>">
+    <link rel="stylesheet"
+          href="<?= site_url("application/assets/js/libs/icons/fontawesome-free-5.10.2-web/css/all.css") ?>">
     <link href="<?= site_url("application/assets/css/tailwindcss.css") ?>" rel="stylesheet">
 
 </head>
-<body>
 
 
+<body style="width: 100%;height: 100%" class="text-sm">
+<div id="menu" class="fixed-top">
+    <nav class="flex items-center justify-between flex-wrap bg-gray-700">
+        <div class="flex items-center flex-shrink-0 text-white mr-6 ml-3">
+            <span class="font-semibold text-xl tracking-tight">atos</span>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div class=" lg:flex-grow p-2">
+                <i class="fas fa-home text-2xl text-white cursor-pointer "></i>
 
-<nav class="z-0 flex items-center justify-between flex-wrap bg-gray-700">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
-             xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
-        </svg>
-        <span class="font-semibold text-xl tracking-tight">atos</span>
-    </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div class="text-sm lg:flex-grow p-2">
-
-                <a href="#responsive-header"
-                   class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4 ">
-                    <i class="fas fa-home text-2xl "></i>
-                </a>
-                <a href="#responsive-header"
-                   class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-                    <i class="fas fa-comments text-2xl"></i>
-                </a>
-                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white">
-                    <i class="fas fa-bell text-2xl"></i>
-                </a>
             </div>
-            <a href="#" class="inline-block
-                                text-sm
-                                 px-3
-                                  py-2
-                                   leading-none
-                                     text-black
-                                      border-white
-                                      bg-gray-700
-                                       hover:border-transparent
-                                         hover:bg-gray-700
-                                           lg:mt-2
-                                           lg:mb-2
-                                           lg:mr-2
-">
+            <div class="float-right mr-6">
+                <i class="fas fa-bell text-2xl text-white cursor-pointer ml-6" id="icone-notificacao"></i>
+                <i class="fas fa-comments text-2xl text-white cursor-pointer ml-6" id="icone-msg"></i>
+                <i class="fas fa-sort-down text-2xl text-white cursor-pointer ml-6" id="icone-opcoes"></i>
+            </div>
+            <!--            <a href="#" class="inline-flex-->
+            <!--                                text-sm-->
+            <!--                                 px-3-->
+            <!--                                  py-2-->
+            <!--                                   leading-none-->
+            <!--                                     text-black-->
+            <!--                                      border-white-->
+            <!--                                      bg-gray-700-->
+            <!--                                       hover:border-transparent-->
+            <!--                                         hover:bg-gray-700-->
+            <!--                                           lg:mt-2-->
+            <!--                                           lg:mb-2-->
+            <!--                                           lg:mr-2-->
+            <!--">-->
 
-<!--<i class="fas fa-user-cog text-2xl hover:text-white dropdown-toggle" data-toggle="dropdown"></i>-->
-                <div>
-                    <button class="block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
-                        <img class="h-full w-full object-cover"
-                             src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
-                             alt="Your avatar"
-                             id="avatar-nav"
-                        >
-                    </button>
+            <div>
 
-                </div>
-            </a>
+            </div>
+            <!--            </a>-->
+        </div>
+    </nav>
+    <div class="float-right mt-1 py-2 w-64 bg-white rounded-lg shadow-xl hidden" id="opcoes-user">
+        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-black">Configurações</a>
+        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-black">Ajuda </a>
+        <a href="<?= site_url("Logado/logout") ?>"
+           class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-black">Sair</a>
     </div>
-</nav>
-<div class="float-right mt-2 py-2 w-48 bg-white rounded-lg shadow-xl hidden" id="nav-user">
-    <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Configurações</a>
-    <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Ajuda </a>
-    <a href="<?= site_url("Logado/logout") ?>" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sair</a>
-</div>
 
+    <div class="float-right mt-2 mr-4 py-2 w-1/3 bg-white rounded-lg shadow-xl hidden" id="nav-msg">
+        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-black"><p>Aqui uma msg</p></a>
+        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-black">Mostrar Mais</a>
+    </div>
+
+    <div class="float-right mt-2 mr-8 py-2 w-1/4 bg-white rounded-lg shadow-xl hidden" id="nav-notificacao">
+        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-black"><p>Aqui uma notificacao</p>
+        </a>
+        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-100 hover:text-black">Mostrar Mais</a>
+    </div>
+</div>
+<div id="corpo" style="width: 100%; height: 100%;">
+    <div class="bg-gray-300 h-75 bg-no-repeat bg-cover  bg-center cursor-pointer"
+         id="fundo-user"
+         style="width: 100%;background-image: url('<?= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTNIhqW7L1KvUWQP8fc1iwcnSG58reMjm8h668ovOuwm2nwTlUx" ?>');">
+        <div class="ml-2 mt-12 fixed-top" >
+            <div class="block w-1/6 h-64  rounded-tl-lg overflow-hidden border-2 border-gray-300 focus:outline-none focus:border-white" >
+                <img class="h-full w-full object-cover "
+                     src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
+                     alt="Your avatar"
+
+                >
+
+            </div>
+            <div class="w-1/6 h-8 bg-black hidden opacity-50 text-white border-2 border-gray-300 rounded-bl-lg  " >
+                <div class="p-1">Clique para alterar</div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 <script type="text/javascript" src="<?= URL_RAIZ() ?>js/home/home.js"></script>
 <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery-3.4.1/jquery.js"></script>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery-1.12.4/jquery-1.12.4.js"></script>
+<script type="text/javascript"
+        src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery-1.12.4/jquery-1.12.4.js"></script>
 <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery-ui-1.12.1/jquery-ui.js"></script>
 <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/vue.js"></script>
 <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js.js"></script>
 <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/mascaras.js"></script>
 <script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/jquery.mask.js"></script>
-<script type="text/javascript" src="<?= URL_RAIZ() ?>application/assets/js/libs/icons/fontawesome-free-5.10.2-web/js/all.js"></script>
+<script type="text/javascript"
+        src="<?= URL_RAIZ() ?>application/assets/js/libs/icons/fontawesome-free-5.10.2-web/js/all.js"></script>
--- js/home/home.js
+++ js/home/home.js
@@ -1,11 +1,52 @@
 $(document).ready(function () {
-    $('form').submit(function (e) {
-        e.preventDefault();
-    });
-    $("#nav-user").hide();
 
-    $("#avatar-nav").click(function(){
-    $("#nav-user").slideToggle();
-})
+
+    $("#corpo").click(function(){
+        if($("#opcoes-user").is(":visible")){
+            $("#opcoes-user").slideUp();
+        }
+        if($("#nav-msg").is(":visible")){
+            $("#nav-msg").slideUp();
+        }
+        if($("#nav-notificacao").is(":visible")){
+            $("#nav-notificacao").slideUp();
+        }
+    });
+    //msg
+    $("#icone-msg").click(function(){
+        $("#nav-msg").slideToggle();
+        if($("#opcoes-user").is(":visible")){
+            $("#opcoes-user").hide();
+        }
+        if($("#nav-notificacao").is(":visible")){
+            $("#nav-notificacao").hide();
+        }
+    });
+    //opcoes
+    $("#icone-opcoes").click(function(){
+        $("#opcoes-user").slideToggle();
+        if($("#nav-msg").is(":visible")){
+            $("#nav-msg").hide();
+        }
+        if($("#nav-notificacao").is(":visible")){
+            $("#nav-notificacao").hide();
+        }
+
+    });
+    //notificacoes
+    $("#icone-notificacao").click(function(){
+        $("#nav-notificacao").slideToggle();
+
+        if($("#nav-msg").is(":visible")){
+            $("#nav-msg").hide();
+        }
+        if($("#opcoes-user").is(":visible")){
+            $("#opcoes-user").hide();
+        }
+
+    })
+
+
 
 });
+
